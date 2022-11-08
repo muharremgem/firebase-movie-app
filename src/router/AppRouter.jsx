29c2f5ff033ece280e64ce5c68a29c5d5a05393e
muharrem gem <muharremgem28@gmail.com>
2022-11-08 23:45:42 +0300

    route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
