14c696d1d6ba754fa3812ee3fdd5db3dfea6641c
muharrem gem <muharremgem28@gmail.com>
2022-11-09 00:02:17 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,10 @@ const Navbar = () => {
   return (
     <nav className=" w-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light">
       <div className="container-fluid w-full flex items-center justify-between px-6">
-        <div
-          className="collapse navbar-collapse flex-grow items-center"
-          id="navbarSupportedContent1"
-        >
-          <a className="text-xl text-white pr-2 font-semibold" href="#">
-            Navbar
-          </a>
-        </div>
+        <a className="text-xl text-white pr-2 font-semibold" href="#">
+          Navbar
+        </a>
+
         {/* Collapsible wrapper */}
         {/* Right elements */}
         <div className="flex items-center relative">
