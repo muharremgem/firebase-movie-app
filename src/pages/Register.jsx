ed757e29d548bd3328d02e619f4285b46cf9160f
muharrem gem <muharremgem28@gmail.com>
2022-11-09 01:01:47 +0300

    handleSubmit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
+import { useState } from "react";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstName, lastName);
+  };
+
   return (
     <div className="flex justify-center">
       <div className="form-image hidden md:block">
@@ -15,7 +26,10 @@ const Register = () => {
         <div
           className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
         >
-          <form className="absolute inset-[2px] rounded-[8px] bg-[#28292d] z-[10] form flex flex-col p-20">
+          <form
+            onSubmit={handleSubmit}
+            className="absolute inset-[2px] rounded-[8px] bg-[#28292d] z-[10] form flex flex-col p-20"
+          >
             <h2 className="text-[#ff4b45] text-2xl font-[500] text-center tracking-[0.1em]">
               Sign Up
             </h2>
@@ -24,6 +38,7 @@ const Register = () => {
                 type="text"
                 required
                 className="relative w-[100%] inputbox-input bg-transparent outline-none text-[#23242a] font-[1em] tracking-[0.05em]"
+                onChange={(e) => setFirstName(e.target.value)}
               />
               <span className="absolute left-0 inputbox-span font-[1em] text-[#8f8f8f] tracking-[0.05em]">
                 First Name
@@ -35,6 +50,7 @@ const Register = () => {
                 type="text"
                 required
                 className="relative w-[100%] inputbox-input bg-transparent outline-none text-[#23242a] font-[1em] tracking-[0.05em]"
+                onChange={(e) => setLastName(e.target.value)}
               />
               <span className="absolute left-0 inputbox-span font-[1em] text-[#8f8f8f] tracking-[0.05em]">
                 Last Name
@@ -46,6 +62,7 @@ const Register = () => {
                 type="email"
                 required
                 className="relative w-[100%] inputbox-input bg-transparent outline-none text-[#23242a] font-[1em] tracking-[0.05em]"
+                onChange={(e) => setEmail(e.target.value)}
               />
               <span className="absolute left-0 inputbox-span font-[1em] text-[#8f8f8f] tracking-[0.05em]">
                 Email
@@ -57,6 +74,7 @@ const Register = () => {
                 type="password"
                 required
                 className="relative w-[100%] inputbox-input bg-transparent outline-none text-[#23242a] font-[1em] tracking-[0.05em]"
+                onChange={(e) => setPassword(e.target.value)}
               />
               <span className="absolute left-0 inputbox-span font-[1em] text-[#8f8f8f] tracking-[0.05em]">
                 Password
