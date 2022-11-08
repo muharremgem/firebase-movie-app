759d6c7c321c74e5fce8345fad25a8cd337ebfa0
muharrem gem <muharremgem28@gmail.com>
2022-11-09 00:00:43 +0300

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+    <nav className=" w-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid w-full flex items-center justify-between px-6">
         <div
           className="collapse navbar-collapse flex-grow items-center"
           id="navbarSupportedContent1"
